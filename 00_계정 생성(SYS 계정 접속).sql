71575d2316a4cf4efc7bfff06307a24954ce7712
kimare <kimarv@naver.com>
2024-02-26 16:50:10 +0900

    20240226_DB 시작

---
+++ 00_계정 생성(SYS 계정 접속).sql
@@ -0,0 +1,25 @@
+-- 한줄 주석 (ctrl + /)
+/* 범위 주석 (ctrl + shift + /) */
+
+/* SQL 1개 실행 : CTRL + ENTER
+ * 
+ * SQL 여러개 실행 : (블럭후) ALT + X
+ * 
+ * */
+
+
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+-- 계정 생성
+CREATE USER KH_KAR IDENTIFIED BY KH1234;
+
+-- 생성된 계정에 
+-- 접속 + 기본 자원 관리 권한 추가
+
+GRANT CONNECT, RESOURCE TO KH_KAR;
+
+--객체 생성 공간 할당
+ALTER USER KH_KAR
+DEFAULT TABLESPACE SYSTEM
+QUOTA UNLIMITED ON SYSTEM;
+
